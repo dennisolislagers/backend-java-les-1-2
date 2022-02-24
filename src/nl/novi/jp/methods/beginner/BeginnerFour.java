dfe494f8b5c6cb260c3c993a9029a56680dccaf9
dennisolislagers <dennis.olislagers@gmail.com>
2022-02-24 12:12:12 +0100

    beginner en junior klaar

---
+++ src/nl/novi/jp/methods/beginner/BeginnerFour.java
@@ -0,0 +1,16 @@
+package nl.novi.jp.methods.beginner;
+
+/**
+ * Maak een methode die jump() heet en de tekst "You jumped" print.
+ *
+ * Roep deze methode vervolgens aan.
+ */
+public class BeginnerFour {
+    public static void main(String[] args) {
+        jumped();
+    }
+
+    public static void jumped() {
+        System.out.println("You jumped");
+    }
+}
